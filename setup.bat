a306b9a692c6669d4ebe4f92721ea5cd6b6b2d71
David Wogan <wogandavid@users.noreply.github.com>
2020-12-22 18:48:24 +0900

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -2,11 +2,11 @@ ECHO Setup will begin...
 
 ECHO Downloading necessary packages from Conda
 
-conda create --name outlookenv python=3.7 networkx datapackage graphviz xlrd glpk
+conda create --prefix ./env python=3.7 networkx datapackage graphviz xlrd glpk
 
 ECHO Packages successfully installed. Activating environment with conda activate outlookenv
 
-conda activate outlookenv
+conda activate ./env
 
 ECHO Environment activated.
 
